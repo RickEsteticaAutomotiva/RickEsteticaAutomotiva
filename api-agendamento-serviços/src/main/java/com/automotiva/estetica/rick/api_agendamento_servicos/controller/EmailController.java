fec74cc497f1b6c7c8e64e121c420cbae59771c7
orodrigoapolo <rodrigo.gsantos@sptech.school>
2025-10-12 11:29:05 -0300

    feat: criando a classe para enviar email

---
+++ api-agendamento-serviços/src/main/java/com/automotiva/estetica/rick/api_agendamento_servicos/controller/EmailController.java
@@ -0,0 +1,32 @@
+package com.automotiva.estetica.rick.api_agendamento_servicos.controller;
+
+import com.automotiva.estetica.rick.api_agendamento_servicos.entity.EmailEntity;
+import com.automotiva.estetica.rick.api_agendamento_servicos.service.EmailService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/emails")
+@AllArgsConstructor
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping("/enviar")
+    public ResponseEntity<String> enviarEmail(@RequestBody EmailEntity email) {
+        return ResponseEntity.ok("Email enviado com sucesso!");
+    }
+
+    @PostMapping("/enviar-com-anexos")
+    public ResponseEntity<String> enviarEmailComAnexos(
+            @RequestPart("email") EmailEntity email,
+            @RequestPart(value = "anexos", required = false) List<MultipartFile> anexos) {
+        emailService.enviarEmailComAnexos(email, anexos);
+        return ResponseEntity.ok("Email com anexos enviado com sucesso!");
+    }
+}
---
+++ api-agendamento-serviços/src/main/java/com/automotiva/estetica/rick/api_agendamento_servicos/enuns/StatusEmailEnum.java
@@ -0,0 +1,7 @@
+package com.automotiva.estetica.rick.api_agendamento_servicos.enuns;
+
+public enum StatusEmailEnum {
+    PROCESSING,
+    SENT,
+    ERROR;
+}
