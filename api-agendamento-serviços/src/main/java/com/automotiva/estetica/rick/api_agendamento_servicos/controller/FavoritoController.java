a22a58fa168a61e4184b7d63f89d16a5a7a7c722
orodrigoapolo <rodrigo.gsantos@sptech.school>
2025-10-20 19:37:55 -0300

    fix: arrumando a uri

--- api-agendamento-serviços/src/main/java/com/automotiva/estetica/rick/api_agendamento_servicos/controller/FavoritoController.java
+++ api-agendamento-serviços/src/main/java/com/automotiva/estetica/rick/api_agendamento_servicos/controller/FavoritoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/favoritos")
+@RequestMapping("/favoritos")
 @RequiredArgsConstructor
 public class FavoritoController {
     private final FavoritoService favoritoService;
