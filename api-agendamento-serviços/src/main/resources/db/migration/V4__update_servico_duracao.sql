95e58778715325ca433690b4c104fe9c9886a9bb
cainaGomesDS <caina.silva@sptech.school>
2026-05-22 22:01:58 -0300

    Revert "feat: adicionado o cache com redis (#44)"
    This reverts commit c822a7c21080ff53802ee2df517e5f985607eb2a.

---
+++ api-agendamento-serviços/src/main/resources/db/migration/V4__update_servico_duracao.sql
@@ -0,0 +1,5 @@
+UPDATE servico SET duracao_minutos = 30 WHERE id = 1;
+UPDATE servico SET duracao_minutos = 60 WHERE id = 2;
+UPDATE servico SET duracao_minutos = 45 WHERE id = 3;
+UPDATE servico SET duracao_minutos = 90 WHERE id = 4;
+UPDATE servico SET duracao_minutos = 240 WHERE id = 5;
